765d54196de4bd9b142aa1040f653f19df6dbc67
Judysoty <coachjudithconsults@gmail.com>
2022-10-18 05:30:43 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by Judysoty
+ * Return: 0
+*/
+
+int main(void)
+{
+
+putchar ('-');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
+
+return (0);
+}
