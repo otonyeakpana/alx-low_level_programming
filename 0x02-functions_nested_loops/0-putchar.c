24127380c55e9d5d8a224fd2a47a1f5e9a680ae4
Judysoty <coachjudithconsults@gmail.com>
2022-10-19 12:51:14 -0700

    tasky task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - prints holberton as a message
  * Return: always 0
 */
 
 int main(void)
 
 {
-putchar ('-');
-putchar ('p');
-putchar ('u');
-putchar ('t');
-putchar ('c');
-putchar ('h');
-putchar ('a');
-putchar ('r');
-putchar ('\n');
+int str[] = { 95, 112, 117, 116, 99, 104, 97, 114};
+int count, sz;
+
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+
+_putchar (str[count]);
+}
+_putchar ('\n');
 
 return (0);
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void)
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-int _putchar(char c);
+extern  void print_alphabet(void);
+extern  void print_alphabet_x10(void);
+extern  int  _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack,  _bauer(void)
+extern  void times _table(void);
+extern int add(int, int);
+extern  void print,_to_98(int n);
+extern void print,_times_table(int n);
+extern int _putchar(char c);
 #endif
