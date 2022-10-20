8861b5f78ea3bd35dab8742e49bc8fec819e0a2c
Judysoty <coachjudithconsults@gmail.com>
2022-10-20 16:42:46 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ *largest_number - returns the largest of 3 numbers
+ *@a:first integer
+ *@b:second integer
+ *@c:third integer
+ *Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-if (a > b)
+int largest;
+if (a > b && a > c)
 {
-if (b > c)
 largest = a;
-else if (a > c)
-largest = a;
-else
+}
+else if (a > b && c > a)
+{
 largest = c;
 }
-else
+else if (b > c)
 {
-if (b > c)
-largest = c;
-else
 largest = b;
 }
-return (largest);
-
+else
+{
+largest = c;
 }
 
+return (largest);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-;
-int convert_day(int month, int day);
-
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+extern int positive_or_negative(int i);
+extern int largest_number(int a, int b, int c);
+extern void print_remaining_days(int month, int day, int year);
+extern int convert_day(int month, int day);
 #endif
