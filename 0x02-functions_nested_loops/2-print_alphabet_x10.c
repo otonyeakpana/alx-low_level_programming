161eef3b6cc38a23633c796bb3b84269fbad2c0f
Judysoty <coachjudithconsults@gmail.com>
2022-10-19 17:58:55 -0700

    mtask2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- 
- *main - print_alphabet x10
+ *print_alphabet_x10 - prints all alphabets in lowercase 10 times.
  *Return: On success 1.
 */
 void print_alphabet_x10(void)
