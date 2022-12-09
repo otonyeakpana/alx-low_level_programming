151e5b6dd4842389bec227494140f1749c5e576f
Sosobae <trulytru404@gmail.com>
2022-12-09 05:57:33 -0800

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * * main - entry point
+ * * Return: alway returns 0
+*/
+int main(void)
+{
+int a;
+int b;
+int c;
+
+for (a = 0; a <= 9 ; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+for (c = 0; c <= 9; c++)
+{
+if (c > b && b > a)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+if (a != 7 || b != 8 || c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
