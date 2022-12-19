560b8eebf2ede978bcc19018945cf4924775226f
Sosobae <trulytru404@gmail.com>
2022-12-19 00:57:55 -0800

    new

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
- *_puts - Prints a string.
- *@str: string.
- */
+ *  * _puts - Prints a string.
+ *   * @str: string.
+*/
 void _puts(char *str)
 {
 while (*str)
 _putchar(*str++);
+
 _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
- *print_rev - Prints a string in reverse.
- *@s: string to tbe reserved.
- */
-void print_rev(char *s)
+ *  * _puts - Prints a string.
+ *   * @str: string.
+*/
+void _puts(char *str)
 {
-int len = 0, i = 0;
-while (s[i++])
-len++;
-for (i = len - 1; i >= 0; i--)
-_putchar(s[i]);
+while (*str)
+_putchar(*str++);
+
 _putchar('\n');
 }
