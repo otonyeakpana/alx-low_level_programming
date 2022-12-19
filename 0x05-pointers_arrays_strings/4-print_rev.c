f44a25cb72fd91528a4a6f9bddc6dba5d074f8b3
Sosobae <trulytru404@gmail.com>
2022-12-19 01:20:43 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,25 @@
-#include "main.h"
-/**
- *  * _puts - Prints a string.
- *   * @str: string.
-*/
-void _puts(char *str)
-{
-while (*str)
-_putchar(*str++);
+include "main.h"
 
-_putchar('\n');
+
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+*/
+
+int main(void)
+	  
+{
+  
+char *str;
+  
+
+  
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+  
+print_rev(str);
+  
+return (0);
+  
 }
