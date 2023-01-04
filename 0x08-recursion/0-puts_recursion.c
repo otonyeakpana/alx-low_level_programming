4f5ed6d9ce52f9bdb2da69e1009362f267a5ddb8
Sosobae <trulytru404@gmail.com>
2023-01-04 00:01:19 -0800

    j

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
  *  * _puts_recursion - prints a string, followed by a new line.
  *   * @s: string
  *    * Return: no return.
- *     */
+*/
 void _puts_recursion(char *s)
 {
 if (*s != '\0')
 {
-putchar(*s);							
-_puts_recursion(s + 1);						
+putchar(*s);
+_puts_recursion(s + 1);
 }
 else
 putchar('\n');
