23b6c61e78a2239db8095dea81951f505838bc90
Sosobae <trulytru404@gmail.com>
2023-01-03 23:50:53 -0800

    g

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
  *  * _strlen_recursion - Returns the length of a string.
  *   * @s: The string to be measured.
  *    * Return: The length of the string.
- *     */
+*/
 int _strlen_recursion(char *s)
 {
 int longit = 0;
