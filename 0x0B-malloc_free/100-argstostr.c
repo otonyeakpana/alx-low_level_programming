d86f2dc95a29195fd81b3c674b00ee8483d0f20f
Sosobae <trulytru404@gmail.com>
2023-01-08 23:18:58 -0800

    arg

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,28 +17,28 @@ for (c = i = 0; i < ac; i++)
 {
 if (av[i] == NULL)
 return (NULL);
-for (j = 0; av[i][j] != '\0'; j++)				
+for (j = 0; av[i][j] != '\0'; j++)
 c++;
 c++;
 }
 aout = malloc((c + 1) * sizeof(char));
 if (aout == NULL)
-{								
+{
 free(aout);
 return (NULL);
 }
 for (i = j = ia = 0; ia < c; j++, ia++)
-{								
+{
 if (av[i][j] == '\0')
-{								
-aout[ia] = '\n';						
-i++;								
-ia++;								
-j = 0;								
+{
+aout[ia] = '\n';
+i++;
+ia++;
+j = 0;
 }
 if (ia < c - 1)
-aout[ia] = av[i][j];						
+aout[ia] = av[i][j];
 }
-aout[ia] = '\0';						
+aout[ia] = '\0';
 return (aout);
 }
