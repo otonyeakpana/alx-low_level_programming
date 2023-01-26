1baba61f9b40d37f5d9ab7e3e48e67c30636dde6
Sosobae <trulytru404@gmail.com>
2023-01-26 08:15:35 -0800

    44

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,21 +1,56 @@
-#include "lists.h"
-#include <strings.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
- * *add_node_end - adds a node at the end of a list
- * *@head:pointer to head
- * *@str:string to duplicate
- * *Return:new element
+ *  * add_node_end - Adds a new node at the end of a list
+ *   * @head: The original linked list
+ *    * @str: The string to add to the node
+ *     * Return: The address of the new list or NULL if it failed
 */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-int len;
-list_t *last;
-list_t *new_node;
-last = *head;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
+list_t *new_list, *temp;
+if (str != NULL)
+{
+new_list = malloc(sizeof(list_t));
+if (new_list == NULL)
 return (NULL);
-for (len = 0; str[len]; len++)
-;
+new_list->str = strdup(str);
+new_list->len = strlen(str);
+new_list->next = NULL;
+if (*head == NULL)
+{
+*head  = new_list;
+return (*head);
+}
+else
+{
+temp = *head;
+while (temp->next)
+temp = temp->next;
+temp->next = new_list;
+return (temp);
+}
+}
+return (NULL);
+}
+/**
+ *  * _strlen - Returns the length of a string
+ *   * @s: String to count
+ *    * Return: String length
+*/
+
+int _strlen(const char *s)
+{
+int c = 0;
+
+while (*s)
+{
+s++;
+c++;
+}
+return (c);
+}
