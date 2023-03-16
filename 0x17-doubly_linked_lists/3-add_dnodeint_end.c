d5a09823c6b997412686df702eccfbc848fc70a4
Sosobae <trulytru404@gmail.com>
2023-03-16 06:23:34 -0700

    3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,28 +1,31 @@
 #include "lists.h"
-
 /**
- *  * add_dnodeint_end - Add a node in the tail
- *   * @head: Pointer to direction of the head
- *    * @n: The data integer
- *     * Return: The direction of the tail node
+ *  * add_dnodeint_end - adds node at end
+ *   * @head: head address of linked list
+ *    * @n: number of new value for new node
+ *     * Return: address of new node or NULL
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *new_mem, *last_dir;
-new_mem = malloc(sizeof(dlistint_t));
-if (new_mem == NULL)
+dlistint_t *tmp, *new;
+
+tmp = *head;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
 return (NULL);
-new_mem->n = n;
-new_mem->next = NULL;
-new_mem->prev = NULL;
-if (*head == NULL)
-*head = new_mem;
-return (new_mem);
+new->next = NULL;
+new->prev = NULL;
+new->n = n;
+if ((*head) == NULL)
+{
+*head = new;
+return (new);
 }
-last_dir = *head;
-while (last_dir->next != NULL)
-last_dir = last_dir->next;
-last_dir->next = new_mem;
-new_mem->prev = last_dir;
-return (new_mem);
+while (tmp->next)
+{
+tmp = tmp->next;
+}
+new->prev = tmp;
+tmp->next = new;
+return (new);
 }
